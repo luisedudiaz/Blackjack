5cbd1ae39d11d6af9fb1c2dd39295a0e78035d6d
Luis Díaz <luis.edu.diaz@outlook.com>
2020-05-24 19:49:57 -0500

    Card Controller

--- server/controllers/Card.js
+++ server/controllers/Card.js
@@ -1,5 +1,23 @@
+const mongoose = require('mongoose')
+const { randomCardNumber } = require('../utils/index')
 const cardController = {}
 
+require('../models/Card')
+const Card = mongoose.model('card')
+
+/**
+ * Return all cards (52 cards).
+ */
 cardController.getCards = (req, res) => {}
+
+/**
+ * Return a random card.
+ */
+cardController.getCard = async (req, res) => {
+  const randomCard = await Card.findOne().skip(randomCardNumber())
+  res.json({
+    randomCard
+  })
+}
 
 module.exports = cardController
