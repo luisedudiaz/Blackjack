d5c5195d0c37e6fb3fe5ff2cc8aeffd8444ac6cf
Daniela Colin <A01371734@itesm.mx>
2020-06-05 17:55:24 -0500

    se cambio el fondo

--- pages/salas/index.vue
+++ pages/salas/index.vue
@@ -1,17 +1,23 @@
 <template>
   <div>
-    <b-table striped hover :items="items" :fields="fields">
-      <template v-slot:cell(name)="data">
-        <h2>
-          {{ data.item.name + data.item.no }}
-        </h2>
-      </template>
-      <template v-slot:cell(no)="data">
-        <NuxtLink :to="`/salas/${data.item.no}`"
-          ><b-button variant="success">{{ data.item.no }}</b-button></NuxtLink
-        >
-      </template>
-    </b-table>
+    <div class="background">
+      <div class="container">
+        <b-table striped hover :items="items" :fields="fields">
+          <template v-slot:cell(name)="data">
+            <h2>
+              {{ data.item.name + data.item.no }}
+            </h2>
+          </template>
+          <template v-slot:cell(no)="data">
+            <NuxtLink :to="`/salas/${data.item.no}`"
+              ><b-button variant="success">{{
+                data.item.no
+              }}</b-button></NuxtLink
+            >
+          </template>
+        </b-table>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,3 +41,39 @@ export default {
   }
 }
 </script>
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.background {
+  /* Location of the image */
+  background-image: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.3) 0%,
+      rgba(0, 0, 0, 0.3) 100%
+    ),
+    url('~assets/cover.jpg');
+  /* Background image is centered vertically and horizontally at all times */
+  background-position: center center;
+
+  /* Background image doesn't tile */
+  background-repeat: no-repeat;
+
+  /* Background image is fixed in the viewport so that it doesn't move when
+       the content's height is greater than the image's height */
+  background-attachment: fixed;
+
+  /* This is what makes the background image rescale based
+       on the container's size */
+  background-size: cover;
+
+  /* Set a background color that will be displayed
+       while the background image is loading */
+  background-color: #464646;
+}
+</style>
